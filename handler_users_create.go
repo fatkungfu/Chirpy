ba65412c5ab9dead84ecc36b73d6855b30223260
Eric Bourcier-Walsh <fatkungfu@gmail.com>
2025-05-27 19:52:20 -0400

    Add user creation and reset functionality with database integration
    - Implement user creation handler and associated SQL queries
    - Add reset handler to clear user data, restricted to development environment
    - Update .gitignore to exclude VSCode files and setup documentation
    - Load environment variables for database connection and platform configuration
    - Enhance database interaction with new queries for user management

---
+++ handler_users_create.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type response struct {
+		User
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	dbUser, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating user", err)
+		return
+	}
+
+	user := User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+
+	respondWithJSON(w, http.StatusCreated, response{
+		User: User{
+			ID:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
+	})
+}
--- main.go
+++ main.go
@@ -1,35 +1,69 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/fatkungfu/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 // apiConfig struct holds any stateful, in-memory data.
 // fileserverHits uses atomic.Int32 for safe concurrent access.
 type apiConfig struct {
 	fileserverHits atomic.Int32 // Uncomment if you want to track hits
+	db             *database.Queries
+	platform       string
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	godotenv.Load()              // Load environment variables from .env file
+	dbURL := os.Getenv("DB_URL") // Get the database URL from environment variables
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	// Get the platform from environment variables
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
+	// Open a connection to the database using the provided URL.
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConn)
+
 	// Create an instance of apiConfig to hold our state.
-	apiCfg := &apiConfig{
+	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{}, // Initialize the atomic counter
+		db:             dbQueries,
+		platform:       platform,
 	}
 
 	// This will act as our HTTP request router. Since we're not
 	// registering any specific paths, it will default to a 404 for all requests.
 	mux := http.NewServeMux()
 	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.Handle("/app/", fsHandler)                                   // Use middleware to wrap the file server handler
-	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp) // Register the validate_chirp handler
-	mux.HandleFunc("GET /healthz", handlerReadiness)                 // Use mux.HandleFunc to register the healthzHandler for the /healthz path.
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)            // Register the metrics handler
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)               // Register the reset handler
+	// Use middleware to wrap the file server handler
+	mux.Handle("/app/", fsHandler)
+	// Register the validate_chirp handler
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	// Use mux.HandleFunc to register the healthzHandler for the /healthz path.
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	// Register the metrics handler
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	// Register the reset handler
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	// We'll configure its address and assign our ServeMux as its handler.
 	server := &http.Server{
@@ -39,6 +73,6 @@ func main() {
 
 	// Use the server's ListenAndServe method to start the server
 	// This will block until the server encounters an error or is shut down.
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
 }
